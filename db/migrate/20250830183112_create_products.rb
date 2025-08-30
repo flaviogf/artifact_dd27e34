40bc05e7be7e9cb59f7e46dcd17da29a7d9cc6b7
flaviogf <flavio.fernandes6@gmail.com>
2025-08-30 15:55:37 -0300

    feat: add product model

---
+++ db/migrate/20250830183112_create_products.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateProducts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :products do |t|
+      t.integer :external_id, index: { unique: true }, null: false
+      t.integer :price_cents, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  context 'validations' do
+    it { is_expected.to validate_numericality_of(:external_id).only_integer.is_greater_than(0) }
+
+    it { is_expected.to validate_numericality_of(:price_cents).only_integer.is_greater_than(0) }
+  end
+end
