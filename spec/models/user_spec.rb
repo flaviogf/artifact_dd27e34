4986830782fda281f258a76cbdc3415805cae372
flaviogf <flavio.fernandes6@gmail.com>
2025-08-30 18:54:08 -0300

    feat: update user definition

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validations' do
-    it { is_expected.to validate_numericality_of(:external_id).only_integer.is_greater_than(0) }
+  context 'associations' do
+    it { is_expected.to have_many(:orders) }
+  end
 
+  context 'validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
 end
