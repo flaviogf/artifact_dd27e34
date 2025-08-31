6e7e15545f7b69c300203dc1a7cf5058eb813db7
flaviogf <flavio.fernandes6@gmail.com>
2025-08-31 13:18:59 -0300

    feat: add reading replica for orders controller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -15,19 +15,21 @@ module Api
           return render json: { error: I18n.t('orders.errors.invalid_per_page') }, status: :bad_request
         end
 
-        orders = Order
-                 .select(
-                   'orders.id AS order_id',
-                   'orders.date AS date',
-                   "to_char(SUM(order_items.price_cents) / 100.0, 'FM999999999.00') AS total",
-                   "json_agg(json_build_object('product_id', order_items.product_id, 'value', to_char(order_items.price_cents / 100.0, 'FM999999999.00'))) AS products"
-                 )
-                 .joins(:order_items)
-                 .group('orders.id', 'orders.date')
-                 .order(:id)
-                 .page(params[:page])
-                 .per(params[:per_page])
-                 .collect { |o| { order_id: o.order_id, date: o.date, total: o.total, products: o.products } }
+        orders = ActiveRecord::Base.connected_to(role: :reading) do
+          Order
+            .select(
+              'orders.id AS order_id',
+              'orders.date AS date',
+              "to_char(SUM(order_items.price_cents) / 100.0, 'FM999999999.00') AS total",
+              "json_agg(json_build_object('product_id', order_items.product_id, 'value', to_char(order_items.price_cents / 100.0, 'FM999999999.00'))) AS products"
+            )
+            .joins(:order_items)
+            .group('orders.id', 'orders.date')
+            .order(:id)
+            .page(params[:page])
+            .per(params[:per_page])
+            .collect { |o| { order_id: o.order_id, date: o.date, total: o.total, products: o.products } }
+        end
 
         render json: orders
       end
