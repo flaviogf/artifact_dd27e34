c6c3477b123b1215b743418026a69c26b2c6a2e3
flaviogf <flavio.fernandes6@gmail.com>
2025-08-31 13:37:03 -0300

    refactor: standardize controllers

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -9,10 +9,7 @@ RSpec.describe 'Api::V1::Products', type: :request do
     it 'returns a list of products' do
       get '/api/v1/products'
 
-      expected_products = products
-                          .sort_by(&:id)
-                          .first(25)
-                          .collect { |p| { 'product_id' => p.id, 'value' => (p.price_cents / 100.0).to_s } }
+      expected_products = build_expected_products(products).first(25)
 
       expect(response.parsed_body).to match_array(expected_products)
     end
@@ -23,10 +20,7 @@ RSpec.describe 'Api::V1::Products', type: :request do
       it 'returns a list of products for the specified page' do
         get '/api/v1/products', params: { page: }
 
-        expected_products = products
-                            .sort_by(&:id)
-                            .slice(25, 25)
-                            .collect { |p| { 'product_id' => p.id, 'value' => (p.price_cents / 100.0).to_s } }
+        expected_products = build_expected_products(products).slice(25, 25)
 
         expect(response.parsed_body).to match_array(expected_products)
       end
@@ -38,10 +32,7 @@ RSpec.describe 'Api::V1::Products', type: :request do
       it 'returns a list of products with the specified per_page' do
         get '/api/v1/products', params: { per_page: }
 
-        expected_products = products
-                            .sort_by(&:id)
-                            .first(per_page)
-                            .collect { |p| { 'product_id' => p.id, 'value' => (p.price_cents / 100.0).to_s } }
+        expected_products = build_expected_products(products).first(per_page)
 
         expect(response.parsed_body).to match_array(expected_products)
       end
@@ -73,6 +64,14 @@ RSpec.describe 'Api::V1::Products', type: :request do
 
         expect(response.parsed_body['error']).to eq('Invalid per_page')
       end
+    end
+
+    private
+
+    def build_expected_products(products)
+      products
+        .sort_by(&:id)
+        .collect { |p| { 'product_id' => p.id, 'value' => format('%0.2f', p.price_cents / 100.0) } }
     end
   end
 end
