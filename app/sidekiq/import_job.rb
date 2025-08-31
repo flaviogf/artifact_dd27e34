f074d004a70860aaccc0100d90933c2476f0b82c
flaviogf <flavio.fernandes6@gmail.com>
2025-08-31 15:10:20 -0300

    refactor: use reader at Import Job

--- app/sidekiq/import_job.rb
+++ app/sidekiq/import_job.rb
@@ -5,35 +5,19 @@ class ImportJob
 
   sidekiq_options queue: :imports, retry: 5
 
+  def initialize(reader: Orders::Reader.new)
+    @reader = reader
+  end
+
   def perform(import_id)
     import = Import.find_by(id: import_id)
     return if import.blank?
 
-    users = {}
-    products = {}
-    orders = {}
-    order_items = {}
+    users, products, orders, order_items = @reader.read(import.file)
 
-    import.file.open do |file|
-      file.each_line do |line|
-        user_id = line[0...10].to_i
-        user_name = line[10...55].strip
-        order_id = line[55...65].to_i
-        product_id = line[65...75].to_i
-        product_price_cents = line[75...87].strip.gsub(/\./, '').to_i
-        order_date = Date.strptime(line[87...95], '%Y%m%d')
-        order_items_id = "#{order_id}-#{product_id}-#{product_price_cents}"
-
-        users[user_id] = { id: user_id, name: user_name }
-        products[product_id] = { id: product_id, price_cents: product_price_cents }
-        orders[order_id] = { id: order_id, user_id: user_id, date: order_date }
-        order_items[order_items_id] = { order_id:, product_id:, price_cents: product_price_cents }
-      end
-    end
-
-    User.upsert_all(users.values)
-    Product.upsert_all(products.values)
-    Order.upsert_all(orders.values)
-    OrderItem.upsert_all(order_items.values, unique_by: %i[order_id product_id price_cents])
+    User.upsert_all(users)
+    Product.upsert_all(products)
+    Order.upsert_all(orders)
+    OrderItem.upsert_all(order_items, unique_by: %i[order_id product_id price_cents])
   end
 end
