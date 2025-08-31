f17caaf4d9e904b806ae95b820095323c934c7a8
flaviogf <flavio.fernandes6@gmail.com>
2025-08-31 18:48:05 -0300

    refactor: user as_json instead of collect

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -21,17 +21,23 @@ module Api
               'orders.id AS order_id',
               'orders.date AS date',
               "to_char(SUM(order_items.price_cents) / 100.0, 'FM999999999.00') AS total",
-              "json_agg(json_build_object('product_id', order_items.product_id, 'value', to_char(order_items.price_cents / 100.0, 'FM999999999.00'))) AS products"
+              <<~SQL.squish
+                json_agg(
+                  json_build_object(
+                    'product_id', order_items.product_id,
+                    'value', to_char(order_items.price_cents / 100.0, 'FM999999999.00')
+                  )
+                ) AS products
+              SQL
             )
             .joins(:order_items)
             .group('orders.id', 'orders.date')
             .order(:id)
             .page(page)
             .per(per_page)
-            .collect { |o| { order_id: o.order_id, date: o.date, total: o.total, products: o.products } }
         end
 
-        render json: orders
+        render json: orders.as_json(except: %i[id])
       end
     end
   end
