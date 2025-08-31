0b3d93262653daf7d9bf19c040f6cdccf61e4b7f
flaviogf <flavio.fernandes6@gmail.com>
2025-08-31 12:00:40 -0300

    test: update factory attributes

--- spec/factories/order_items.rb
+++ spec/factories/order_items.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :order_item, class: OrderItem
+  factory :order_item, class: OrderItem do
+    price_cents { Faker::Number.number(digits: 5) }
+  end
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :order, class: Order
+  factory :order, class: Order do
+    user { create(:user) }
+    date { Faker::Date.backward(days: 30) }
+  end
 end
